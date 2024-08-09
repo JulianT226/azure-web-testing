676d15608b72a030c4b3a537168c8bdff86ac34c
APVI_git? <julian.g.tapia@gmail.com>
2024-08-09 16:56:34 -0400

    plsssssss

--- server.js
+++ server.js
@@ -27,4 +27,4 @@ app.post('/process-form', async (req, res) => {
   }
 });
 
-app.listen(3000, () => console.log('Server running on port 3000'));
+app.listen(process.env.PORT || 3000, () => console.log(`Server running on port ${process.env.PORT || 3000}`));
